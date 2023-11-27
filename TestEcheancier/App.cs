1758333c4ef9aef0ec2b4aed095b5d0ffcbacd77
Ass <sambassane1997@gmail.com>
2023-11-27 11:30:45 +0000

    Test backend

---
+++ TestEcheancier/App.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Threading.Tasks;
+using TestEcheancier.Models;
+
+namespace TestEcheancier
+{
+    public class App
+    {
+
+        public void Run(Arguments arguments)
+        {
+
+            Console.WriteLine($"On doit avoir {arguments.NBEcheance} echeance(s)");
+            for (int i = 1; i <= arguments.NBEcheance; i++)
+            {
+                var previous = arguments.DateDebut.AddMonths((i - 1) * arguments.Periodicite);
+                var res = (i == arguments.NBEcheance && arguments.DateFin != null) ? arguments.DateFin.Value : arguments.DateDebut.AddMonths(i * arguments.Periodicite).AddDays(-1);
+                Console.Write($"du {previous.ToShortDateString()} au {res.ToShortDateString()} pour un montant {Math.Round(arguments.MontantAnnuel / arguments.NBEcheance,2)} euros \n");
+            }
+
+
+        }
+    }
+}
